4b1797a2da8d672ae2fe7e3414753f7b2864a99f
Oleksandr Pushyshyn <o.pushyshyn@gmail.com>
2024-07-24 17:56:22 +0300

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+import './buttonBackToTop.png'
+import './footerLogo.png'
+import footerLogo from './footerLogo.png'
+import buttonBackToTop from './buttonBackToTop.png'
+
+const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <a href="https://github.com/fs-apr24-Tech-Titans" className={styles.logoLink}>
+              <img src={footerLogo} alt="logo" className={styles.logo} />
+        </a>
+        <nav className={styles.nav}>
+          <a
+            href="https://github.com/fs-apr24-Tech-Titans"
+            className={styles.item}
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://github.com/fs-apr24-Tech-Titans"
+            className={styles.item}
+            target="_blank"
+          >
+            Contact
+          </a>
+          <a
+            href="https://github.com/fs-apr24-Tech-Titans"
+            className={styles.item}
+            target="_blank"
+          >
+            Rights
+          </a>
+        </nav>
+        <div className={styles.backToTop}  onClick={scrollToTop}>
+          <p className={styles.backToTopText}>Back to top</p>
+          <button
+            aria-label="Scroll to top"
+            type="button"
+            id="back-to-top"
+            onClick={scrollToTop}
+            className={styles.backToTopButton}
+          >
+            <img
+              src={buttonBackToTop}
+              alt="back to top"
+              className={styles.backToTopIcon}
+            />
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
