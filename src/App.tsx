a7ee68fc675daa28522c4e107ae3378e113af5dc
Yulia Pel <pyuliya2016@gmail.com>
2024-07-25 16:46:04 +0300

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import { PageNotFound } from "./components/PageNotFound";
+import { Burger } from "./components/Burger/Burger";
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Outlet />
       <Footer />
       <PageNotFound />
+      <Burger />
     </>
   );
 };
