b04168b4d6d8491c845ddb31ab3c94924c2e2662
Oleksandr Pushyshyn <o.pushyshyn@gmail.com>
2024-07-26 10:16:21 +0300

    add cartItem

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import { PageNotFound } from "./components/PageNotFound";
 import { Burger } from "./components/Burger/Burger";
+import { CartItem } from "./components/CartItem";
 
 
 export const App: React.FC = () => {
@@ -14,7 +15,8 @@ export const App: React.FC = () => {
       <Header />
       <ProductCard />
       <Outlet />
-      <Footer />
+        <Footer />
+        <CartItem/>
       <PageNotFound />
       <Burger />
     </>
