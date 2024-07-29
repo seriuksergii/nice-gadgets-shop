e043c1823ab28b181cc4fd017dd4f978ce0c7d8f
Yulia Pel <pyuliya2016@gmail.com>
2024-07-29 13:13:30 +0300

    edit header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Header } from './components/Header';
 import { Outlet } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
 import { SliderSwiper } from './components/SliderSwiper';
-import { PageNotFound } from './components/PageNotFound';
 
 
 export const App: React.FC = () => {
@@ -14,7 +13,6 @@ export const App: React.FC = () => {
         <main className='main'>
          <SliderSwiper />
         <Outlet />
-        <PageNotFound />
       </main>
         <Footer />
     </>
