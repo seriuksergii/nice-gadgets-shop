abbb8a47bd29509ae4fadf86c1da3a485ea28376
Oleksandr Pushyshyn <o.pushyshyn@gmail.com>
2024-07-30 18:34:16 +0300

    add icon

--- src/components/FavAndCartBlock/FavAndCartBlock.tsx
+++ src/components/FavAndCartBlock/FavAndCartBlock.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './FavAndCartBlock.scss';
+import { Link } from 'react-router-dom';
 
 export const FavAndCartBlock: React.FC = () => {
   return (
@@ -7,13 +8,19 @@ export const FavAndCartBlock: React.FC = () => {
       <div className="info-buttons__icon info-buttons__icon--favorites">
         <a className='info-buttons__link' href="#">
           <img src="/img/icons/favourites.svg" alt="" />
+          <span className="count">
+            <p className='countText'>1</p>
+          </span>
         </a>
       </div>
 
       <div className="info-buttons__icon info-buttons__icon--cart">
-        <a className='info-buttons__link' href="#">
+        <Link className='info-buttons__link' to="/cartPage">
           <img src="/img/icons/shopping-bag.svg" alt="" />
-        </a>
+          <span className="count">
+            <p className='countText'>1</p>
+          </span>
+        </Link>
       </div>
     </div>
   );
