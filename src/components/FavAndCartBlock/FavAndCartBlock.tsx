ec56ae687c0271a76553d1bf9379bc934353b7a6
Yulia Pel <pyuliya2016@gmail.com>
2024-07-31 11:55:58 +0300

    add functionalit

--- src/components/FavAndCartBlock/FavAndCartBlock.tsx
+++ src/components/FavAndCartBlock/FavAndCartBlock.tsx
@@ -2,31 +2,40 @@ import React from 'react';
 import './FavAndCartBlock.scss';
 import { NavLink } from 'react-router-dom';
 import { getNavlinkStyle } from '../../services/styleHelpers';
+import { useUserActions } from '../../Contexts/useUserActions';
 
 export const FavAndCartBlock: React.FC = () => {
+  const { userAction } = useUserActions();
+  const { cart, favorites } = userAction;
+
+  const cartCount = cart.length;
+  const favoritesCount = favorites.length;
+
   return (
     <div className="info-buttons">
-        <NavLink
-          to={'/favorites'}
-          className={({ isActive }) => getNavlinkStyle(isActive, 'info-buttons__link')}
-        >
-          <img className='icon' src="/img/icons/favourites.svg" alt="favorites" />
-         <span className="count">
-            <p className='countText'>1</p>
+      <NavLink
+        to={'/favorites'}
+        className={({ isActive }) => getNavlinkStyle(isActive, 'info-buttons__link')}
+      >
+        <img className="icon" src="/img/icons/favourites.svg" alt="favorites" />
+        {favoritesCount > 0 && (
+          <span className="count">
+            <p className="countText">{favoritesCount}</p>
           </span>
-        </NavLink>
-     
+        )}
+      </NavLink>
 
-      
-        <NavLink
-          to={'/cart'}
-          className={({ isActive }) => getNavlinkStyle(isActive, 'info-buttons__link')}
-        >
-          <img className='icon' src="/img/icons/shopping-bag.svg" alt="cart" />
-         <span className="count">
-            <p className='countText'>1</p>
+      <NavLink
+        to={'/cart'}
+        className={({ isActive }) => getNavlinkStyle(isActive, 'info-buttons__link')}
+      >
+        <img className="icon" src="/img/icons/shopping-bag.svg" alt="cart" />
+        {cartCount > 0 && (
+          <span className="count">
+            <p className="countText">{cartCount}</p>
           </span>
-        </NavLink>
+        )}
+      </NavLink>
     </div>
   );
 };
