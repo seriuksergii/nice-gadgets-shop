d64f163cbbf76f589744d381052db576a6179e95
Illia Kocherovets <illiakocherovets@gmail.com>
2024-08-01 14:11:28 +0200

    add buttons & prices with some UI fixxes

--- src/components/TechSpecs/TechSpecs.tsx
+++ src/components/TechSpecs/TechSpecs.tsx
@@ -1,8 +1,11 @@
+// TechSpecs.tsx
 import React from 'react';
 import './TechSpecs.scss';
 import { Product } from '../../types';
 
-type TechSpecsProps = Pick<Product, 'screen' | 'resolution' | 'processor' | 'ram' | 'camera' | 'zoom' | 'cell'>;
+type TechSpecsProps = Pick<Product, 'screen' | 'resolution' | 'processor' | 'ram' | 'camera' | 'zoom' | 'cell'> & {
+  fullSpecs?: boolean;
+};
 
 export const TechSpecs: React.FC<TechSpecsProps> = ({
   screen,
@@ -12,28 +15,37 @@ export const TechSpecs: React.FC<TechSpecsProps> = ({
   camera,
   zoom,
   cell,
+  fullSpecs = true,
 }) => {
   return (
     <section className="tech-specs">
-      <h3 className="tech-specs__title">Tech specs</h3>
+      {fullSpecs && <h3 className="tech-specs__title">Tech specs</h3>}
       <div className="tech-specs__description">
         <div className="tech-specs__labels">
           <span>Screen</span>
           <span>Resolution</span>
           <span>Processor</span>
           <span>RAM</span>
-          <span>Camera</span>
-          <span>Zoom</span>
-          <span>Cell</span>
+          {fullSpecs && (
+            <>
+              <span>Camera</span>
+              <span>Zoom</span>
+              <span>Cell</span>
+            </>
+          )}
         </div>
         <div className="tech-specs__values">
           <span>{screen}</span>
           <span>{resolution}</span>
           <span>{processor}</span>
           <span>{ram}</span>
-          <span>{camera}</span>
-          <span>{zoom}</span>
-          <span>{cell.join(', ')}</span>
+          {fullSpecs && (
+            <>
+              <span>{camera}</span>
+              <span>{zoom}</span>
+              <span>{cell.join(', ')}</span>
+            </>
+          )}
         </div>
       </div>
     </section>
