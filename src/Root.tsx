b39b6434867ba5b05a72cc37832788e87a2aedcc
Oleksandr Spyrydonov <spiridonovsasa731@gmail.com>
2024-08-02 18:21:04 +0300

    Add theme changer

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,7 @@ import { PageNotFound } from './components/PageNotFound/PageNotFound';
 import { Cart } from './components/Cart/Cart';
 import { ProductPage } from './components/ProductPage';
 import { ActionUserProvider } from './Contexts/UserActionProvider';
+import { ThemeProvider } from './Contexts/ThemeContext';
 import { HomePage } from './pages/HomePage';
 import { PhonePage } from './pages/PhonePage';
 import { FavouritesPage } from './pages/FavouritesPage';
@@ -14,22 +15,24 @@ import AboutUs from './components/AboutUs/AboutUs';
 
 export const Root = () => (
   <Router>
-    <ActionUserProvider>
-      <React.StrictMode>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="phone" element={<PhonePage />} />
-            <Route path="tablets" element={<TabletsPage />} />
-            <Route path="accessories" element={<AccessoriesPage/>} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="favorites" element={<FavouritesPage />} />
-            <Route path=":category/:itemId" element={<ProductPage />} />
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="aboutUs" element={<AboutUs />} />
-          </Route>
-        </Routes>
-      </React.StrictMode>
-    </ActionUserProvider>
+    <ThemeProvider>
+      <ActionUserProvider>
+        <React.StrictMode>
+          <Routes>
+            <Route element={<App />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="phone" element={<PhonePage />} />
+              <Route path="tablets" element={<TabletsPage />} />
+              <Route path="accessories" element={<AccessoriesPage />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="favorites" element={<FavouritesPage />} />
+              <Route path=":category/:itemId" element={<ProductPage />} />
+              <Route path="*" element={<PageNotFound />} />
+              <Route path="aboutUs" element={<AboutUs />} />
+            </Route>
+          </Routes>
+        </React.StrictMode>
+      </ActionUserProvider>
+    </ThemeProvider>
   </Router>
 );
