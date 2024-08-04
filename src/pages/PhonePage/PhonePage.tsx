3951737156743f6dedc23014a783ff5c537c2636
Sergii Serdiuk <162016576+seriuksergii@users.noreply.github.com>
2024-08-04 20:33:10 +0300

    add-translate

--- src/pages/PhonePage/PhonePage.tsx
+++ src/pages/PhonePage/PhonePage.tsx
@@ -1,4 +1,8 @@
 import './PhonePage.scss';
 import { ProductsPage } from '../ProductsPage';
+import { useTranslation } from 'react-i18next';
 
-export const PhonePage: React.FC = () => <ProductsPage category={'phones'} title="Mobile phones" />;
+export const PhonePage: React.FC = () => {
+  const { t } = useTranslation();
+  return <ProductsPage category={'phones'} title={t('phone_page.title')} />;
+};
