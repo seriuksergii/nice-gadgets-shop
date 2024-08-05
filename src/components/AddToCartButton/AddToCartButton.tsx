a1581d8faa9460fab28c3308c7840bb88a114240
Sergii Serdiuk <162016576+seriuksergii@users.noreply.github.com>
2024-08-05 20:25:22 +0300

    add pop-up

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -5,6 +5,7 @@ import { SimpleProduct } from '../../types';
 import { useUserActions } from '../../Contexts/useUserActions';
 import { ActionTypes } from '../../Contexts/reduser';
 import { useTranslation } from 'react-i18next';
+import toast from 'react-hot-toast';
 interface Props {
  product: SimpleProduct
 }
@@ -21,13 +22,33 @@ export const AddToCartButton: React.FC<Props> = ({ product }) => {
     dispatch({ type: ActionTypes.onAddToCart, payload: product });
   };
 
+  const notify = () => toast.success('Product added to cart!', {
+    style: {
+      border: '5px solid #27AE60',
+      borderRadius: '20px',
+      padding: '25px 30px 25px 30px',
+      color: '#fff',
+      backgroundColor: '#27AE60',
+      fontSize: '20px',
+    },
+    iconTheme: {
+      primary: '#fff',
+      secondary: '#27AE60',
+    },
+  });
+
+  const handleClick = () => {
+    handlerOnAddToCart();
+    notify();
+  };
+
 
   return (
     <button
       className={cn('button', {
         "button--disabled": isInCart,
       })}
-      onClick={handlerOnAddToCart}
+      onClick={handleClick}
       disabled={isInCart}
     >
       {isInCart ? t('product_card.added') : t('product_card.add')}
