f3f5032a0adaa0d90f8e705225cacd5d727c89a9
Oleksandr Spyrydonov <spiridonovsasa731@gmail.com>
2024-08-05 19:13:53 +0300

    Fix

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ProductCard.scss';
 import { Link } from 'react-router-dom';
 
-import { Product} from '../../types';
+import { Product } from '../../types';
 
 import { AddToCartButton } from '../AddToCartButton';
 import { AddToFavButton } from '../AddToFavButton';
@@ -21,40 +21,40 @@ export const ProductCard: React.FC<Props> = ({ product }) => {
   const URL = `/${category}/${itemId}`;
 
   return (
-     <article className="productCard">
-        <Fade triggerOnce={true}>
+    <article className="productCard">
+      <Fade triggerOnce={true}>
         <Link to={URL}>
-           <Zoom triggerOnce={true}>
-              <img className="productCard__image" src={image} alt={name} />
-              </Zoom>
-        <p className="productCard__title">{name}</p>
-        <div className="productCard__prices">
-          <span className="productCard__prices-discount">${price}</span>
-          <span className="productCard__prices-full">${fullPrice}</span>
-        </div>
-      </Link>
-      <div className="productCard__params">
-        <div className="productCard__params-pair">
-          <p className="productCard__param">{t('product_card.screen')}</p>
-          <p className="productCard__value">{screen}</p>
+          <Zoom triggerOnce={true}>
+            <img className="productCard__image" src={image} alt={name} />
+          </Zoom>
+          <p className="productCard__title">{name}</p>
+          <div className="productCard__prices">
+            <span className="productCard__prices-discount">${price}</span>
+            <span className="productCard__prices-full">${fullPrice}</span>
+          </div>
+        </Link>
+        <div className="productCard__params">
+          <div className="productCard__params-pair">
+            <p className="productCard__param">{t('product_card.screen')}</p>
+            <p className="productCard__value">{screen}</p>
+          </div>
+
+          <div className="productCard__params-pair">
+            <p className="productCard__param">{t('product_card.сapacity')}</p>
+            <p className="productCard__value">{capacity}</p>
+          </div>
+
+          <div className="productCard__params-pair">
+            <p className="productCard__param">{t('product_card.ram')}</p>
+            <p className="productCard__value">{ram}</p>
+          </div>
         </div>
 
-        <div className="productCard__params-pair">
-          <p className="productCard__param">{t('product_card.сapacity')}</p>
-          <p className="productCard__value">{capacity}</p>
+        <div className="productCard__buttons">
+          <AddToCartButton product={product} />
+          <AddToFavButton product={product} />
         </div>
-
-        <div className="productCard__params-pair">
-          <p className="productCard__param">{t('product_card.ram')}</p>
-          <p className="productCard__value">{ram}</p>
-        </div>
-      </div>
-
-      <div className="productCard__buttons">
-        <AddToCartButton product={product} />
-        <AddToFavButton product={product} />
-        </div>
-        </Fade>
+      </Fade>
     </article>
   );
 };
